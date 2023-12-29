4e19021b75d8fb7aae66147fc8ef1ef374a85dc9
Alex Cyr <alexcyr@Alexs-MacBook-Pro.local>
2023-12-29 01:13:24 -0500

    galciv page plus blackstar and diner banners

--- src/assets/data/projects/blackStar.ts
+++ src/assets/data/projects/blackStar.ts
@@ -3,6 +3,8 @@ import { MediaType } from "types/mediaType";
 import { Project } from "types/project";
 import { Size } from "types/size";
 
+import Banner from "assets/images/black-star/banner3.jpeg";
+
 import GameVid from "assets/video/black-star/capture.mp4";
 import GameScreen from "assets/images/black-star/screen.jpg";
 
@@ -22,6 +24,17 @@ export const blackStar: Project = {
 			type: ContentType.text,
 			text: `"Black Star" is a mobile game built with Unity where players draw cards featuring colored shapes. The objective is to strategically place cards on a grid, creating three-in-a-row sequences of matching colors and shapes, similar to tic-tac-toe. With strategic removal cards and wildcard black stars, the game combines skill and surprise for a dynamic and challenging experience. `,
 			size: Size.lg,
+		},
+		{
+			type: ContentType.multimedia,
+			size: Size.lg,
+			media: [
+				{
+					type: MediaType.image,
+					src: Banner,
+					alt: "black star icon banner",
+				},
+			],
 		},
 		{
 			type: ContentType.multimedia,
--- src/assets/data/projects/galCiv.ts
+++ src/assets/data/projects/galCiv.ts
@@ -3,6 +3,13 @@ import { MediaType } from "types/mediaType";
 import { Project } from "types/project";
 import { Size } from "types/size";
 
+import Banner from "assets/images/gal-civ/banner.jpeg";
+import GalCiv1 from "assets/images/gal-civ/galciv_3.jpg";
+import GalCiv2 from "assets/images/gal-civ/galciv_4.jpg";
+import GalCiv3 from "assets/images/gal-civ/galciv_5.jpg";
+
+import Ipad from "assets/images/gal-civ/galciv_22.jpg";
+
 export const galCiv: Project = {
 	id: "gal-civ",
 	title: "Galactic Civilizations III: Ship Builder",
@@ -17,7 +24,7 @@ export const galCiv: Project = {
 		},
 		{
 			type: ContentType.text,
-			text: `In collaboration with Stardock game studios, my team and I had the privilege of adapting the ship editor from Galactic Civilizations III for a mobile iPad application as part of our college capstone course. Over a three-month span, my role as a UI designer was centered on refining the shipbuilding process. Key enhancements include highlighting essential elements, clear indicators for attaching parts, and an intuitive touch-based rotate UI, ensuring an optimal and immersive editing experience on the iPad.`,
+			text: `In collaboration with Stardock game studios, my team and I had the privilege of adapting the ship editor from Galactic Civilizations III for a mobile iPad application as part of our college capstone course. `,
 			size: Size.lg,
 		},
 		{
@@ -25,10 +32,47 @@ export const galCiv: Project = {
 			size: Size.lg,
 			media: [
 				{
-					type: MediaType.video,
-					src: "/video.mp4",
-					alt: "video description",
-					caption: "optional caption",
+					type: MediaType.image,
+					src: Banner,
+					alt: "gal civ III banner",
+				},
+			],
+		},
+		{
+			type: ContentType.multimedia,
+			size: Size.xs,
+			media: [
+				{
+					type: MediaType.image,
+					src: Ipad,
+					alt: "gal civ III ipad",
+				},
+			],
+		},
+		{
+			type: ContentType.text,
+			text: `Over a three-month span, my role as a UI designer was centered on refining the shipbuilding process. Key enhancements include highlighting essential elements, clear indicators for attaching parts, and an intuitive touch-based rotate UI, ensuring an optimal and immersive editing experience on the iPad.`,
+			size: Size.md,
+		},
+		{
+			type: ContentType.multimedia,
+			size: Size.sm,
+			isGrid: true,
+			media: [
+				{
+					type: MediaType.image,
+					src: GalCiv1,
+					alt: "gal civ III create ship",
+				},
+				{
+					type: MediaType.image,
+					src: GalCiv3,
+					alt: "gal civ III rotate ui",
+				},
+				{
+					type: MediaType.image,
+					src: GalCiv2,
+					alt: "gal civ III ship select",
 				},
 			],
 		},
