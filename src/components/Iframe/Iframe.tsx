b3e9c70c846e6778692e66380f603bc2b8b21100
Alex Cyr <alexcyr@Alexs-MacBook-Pro.local>
2024-03-10 01:46:24 -0500

    iframe style

--- src/components/Iframe/Iframe.tsx
+++ src/components/Iframe/Iframe.tsx
@@ -1,3 +1,4 @@
+import { PlayIcon } from "components/Icons/Play";
 import { IframeContext } from "pages/Project/Project";
 import { useContext, useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
@@ -17,6 +18,7 @@ const IframeWindow = styled.iframe`
 	width: 100%;
 	max-height: 100%;
 	aspect-ratio: 1/1;
+	border: none;
 `;
 
 const LoadingWrapper = styled.div`
@@ -25,14 +27,13 @@ const LoadingWrapper = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	font-size: ${({ theme }) => theme.text.size.s32};
 
 	&::after {
 		content: "";
 		position: absolute;
 		inset: 0;
 		background: ${({ theme }) => theme.color.primary1};
-		opacity: 0.3;
+		opacity: 0.1;
 	}
 `;
 
@@ -44,15 +45,64 @@ const LoadIframeButton = styled.button`
 	flex-direction: column;
 	gap: 4px;
 	text-align: center;
+	align-items: center;
+	justify-content: center;
 	position: absolute;
 	inset: 0;
+	color: ${({ theme }) => theme.color.primary1};
+	gap: ${({ theme }) => theme.space.s32};
+
+	span {
+		font-family: ${({ theme }) => theme.text.family.body};
+		font-size: ${({ theme }) => theme.text.size.s16};
+		font-weight: 300;
+		letter-spacing: 1px;
+
+		strong {
+			font-family: ${({ theme }) => theme.text.family.title};
+			font-size: ${({ theme }) => theme.text.size.s28};
+			font-weight: 600;
+			letter-spacing: 2px;
+		}
+	}
+	
+
 	&::after {
 		content: "";
 		position: absolute;
 		inset: 0;
 		background: ${({ theme }) => theme.color.primary1};
-		opacity: 0.3;
+		opacity: 0.1;
 	}
+
+	&:hover {
+		transition: all 0.25s ease-in-out;
+		&::after {
+			background: ${({ theme }) => theme.color.primary2};
+			transition: all 0.25s ease-in-out;
+		}
+		svg {
+			transition: all 0.15s ease-in-out;
+			transform: scale(1.05);
+		}
+
+		span {
+			transition: all 3s ease-in-out;
+			font-size: ${({ theme }) => theme.text.size.s18};
+
+			strong {
+				transition: all 3s ease-in-out;
+				font-size: ${({ theme }) => theme.text.size.s32};
+			}
+		}
+	}
+`;
+
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-transform: uppercase;
+  gap: ${({ theme }) => theme.space.s4};
 `;
 
 export const Iframe = ({ src, alt, preload }: IframeProps) => {
@@ -84,10 +134,13 @@ export const Iframe = ({ src, alt, preload }: IframeProps) => {
 					/>
 				) : (
 					<LoadIframeButton onClick={() => setIframeId(alt)} title={`load ${alt}`}>
+						<PlayIcon />
+						<TextWrapper>
 						<span>
-							<strong>Load Iframe</strong>
+							<strong>Play Web App</strong>
 						</span>
-						<span>Running can be computer intensive</span>
+						<span>Attn: Can be computer intensive</span>
+						</TextWrapper>
 					</LoadIframeButton>
 				)
 			) : null}
